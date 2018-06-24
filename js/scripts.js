07a304026d0a04d54e34ba8950d592e25f3254c5
Leah Brunetto <leahbrunetto@gmail.com>
2018-06-23 21:06:17 -0400

    Got all four filters working

--- js/scripts.js
+++ js/scripts.js
@@ -12,7 +12,7 @@ for (var i = 0; i < modalLinks.length; i++) {
 	  e.preventDefault();
 	  var modalNumber = this.getAttribute('data-modal'); // Find the modal link's data attribute value   
 	  var modalElement = document.getElementById(modalNumber); // Find the DOM element with that same ID
-	  //modalElement.classList.add('open'); // Add the "open" class to that DOM element so that it displays on the page
+	  modalElement.classList.add('open'); // Add the "open" class to that DOM element so that it displays on the page
 	});
 }
 
@@ -42,14 +42,17 @@ for (var j = 0; j < closeButtons.length; j++) { // Choose a variable other than 
 // }
 
 // Get individual input - "easy" solution
+var allProjectsInput = document.getElementById('filter0');
 var webDevInput = document.getElementById('filter1');
+var graphicDesignInput = document.getElementById('filter2');
+var illustrationInput = document.getElementById('filter3');
 var projects = document.getElementsByClassName('project');
+
+
 webDevInput.addEventListener('change', function() {
-	console.log('changed!');	
 	for (var k=0; k < projects.length; k++) {
 		// Does the proejct have a data-category === "1"?
 		var projectCategory = projects[k].getAttribute('data-category');
-		console.log(projectCategory);
 		if (projectCategory === "1") {
 			projects[k].classList.add('active');
 		} else {
@@ -59,5 +62,78 @@ webDevInput.addEventListener('change', function() {
 	}
 });
 
+graphicDesignInput.addEventListener('change', function() {
+	for (var l=0; l < projects.length; l++) {
+		// Does the proejct have a data-category === "2"?
+		var projectCategory = projects[l].getAttribute('data-category');
+		if (projectCategory === "2") {
+			projects[l].classList.add('active');
+		} else {
+			//No match, remove the class!
+			projects[l].classList.remove('active');
+		}
+	}
+});
 
+illustrationInput.addEventListener('change', function() {
+	for (var m=0; m < projects.length; m++) {
+		// Does the proejct have a data-category === "3"?
+		var projectCategory = projects[m].getAttribute('data-category');
+		if (projectCategory === "3") {
+			projects[m].classList.add('active');
+		} else {
+			//No match, remove the class!
+			projects[m].classList.remove('active');
+		}
+	}
+});
 
+allProjectsInput.addEventListener('change', function() {
+	for (var n=0; n < projects.length; n++) {
+		projects[n].classList.add('active');
+	}
+});
+
+// /* FILTERING SOLUTION 2:
+//     * This solution allows us to loop through a set of filters and projects
+//     * by class name, which is more flexible. We'll make use of a closure
+//     * to fix a scoping issue.
+//     */
+//   // Get all filter input elements
+//   var filters = document.getElementsByClassName('portfolio-filter');
+//   // Get all project inputs
+//   var projects = document.getElementsByClassName('project');
+//
+//   // Loop through each filter to add an event listener
+//   for (var k = 0; k < filters.length; k++) {
+//
+//     // Use a closure to pass the value of k into the callback as filterIndex
+//     (function(filterIndex) {
+//
+//       filters[filterIndex].addEventListener('change', function() {
+//
+//         // When a filter changes, loop through projects
+//         for (var m = 0; m < projects.length; m++) {
+//
+//           // Get the category from each project
+//           var projectCategory = projects[m].getAttribute('data-category');
+//
+//           if (filterIndex === 0) {
+//             // Someone clicked 'All', so every project should be active
+//             projects[m].classList.add('active');
+//
+//           } else if (projectCategory === filterIndex.toString()) {
+//             // It matches! Let's activate. Note that we convert m to a
+//             // string so we can use ===, which tests if the two
+//             // variables are equal in value AND type
+//             projects[m].classList.add('active');
+//
+//           } else {
+//             // No match -- remove the class!
+//             projects[m].classList.remove('active');
+//           }
+//         }
+//       });
+//     })(k);
+//   }
+// })();
